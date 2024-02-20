f66584876c2e04e34448c7c010efca88362ee874
VeselinaTencheva <veselinatencheva9@gmail.com>
2024-02-20 12:37:55 +0200

    find diagnose by category

--- src/main/java/university/medicalrecordsdemo/repository/DiagnosisRepository.java
+++ src/main/java/university/medicalrecordsdemo/repository/DiagnosisRepository.java
@@ -1,10 +1,14 @@
 package university.medicalrecordsdemo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import university.medicalrecordsdemo.model.entity.AppointmentEntity;
+import university.medicalrecordsdemo.model.entity.DepartmentType;
 import university.medicalrecordsdemo.model.entity.DiagnosisEntity;
 
 public interface DiagnosisRepository extends JpaRepository<DiagnosisEntity, Long> {
     DiagnosisEntity findByAppointments(AppointmentEntity appointment);
+    List<DiagnosisEntity> findAllByCategory(DepartmentType category);
 }

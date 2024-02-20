f61aa69cf8ad9e60f19ca0348d2e36a1c6fd4b5d
INRIX-Veselina-Tencheva <veselina.tencheva@inrix.com>
2024-02-20 12:28:52 +0200

    find all physician by specialty

--- src/main/java/university/medicalrecordsdemo/repository/PhysicianRepository.java
+++ src/main/java/university/medicalrecordsdemo/repository/PhysicianRepository.java
@@ -1,8 +1,13 @@
 package university.medicalrecordsdemo.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import university.medicalrecordsdemo.model.entity.PhysicianEntity;
+import university.medicalrecordsdemo.model.entity.SpecialtyType;
 
 public interface PhysicianRepository extends JpaRepository<PhysicianEntity, Long> {
+    List<PhysicianEntity> findAllBySpecialtiesIn(Set<SpecialtyType> specialties);
 }
